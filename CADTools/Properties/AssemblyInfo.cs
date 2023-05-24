37120a841f62603b32440c87716db793b11145ab
mjensen61 <43187944+mjensen61@users.noreply.github.com>
2023-05-24 10:03:18 +1000

    Amended version number

--- CADTools/Properties/AssemblyInfo.cs
+++ CADTools/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2023.05.23.01")]
-[assembly: AssemblyFileVersion("2022.05.23.01")]
+[assembly: AssemblyVersion("2023.05.24.01")]
+[assembly: AssemblyFileVersion("2022.05.24.01")]
